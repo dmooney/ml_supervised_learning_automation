6b0b48cd5fc01018c93297eaa0f9520a9c0c5b6a
David Mooney <dmooney@dmooney.org>
2014-09-21 10:11:33 -0400

    Dropped laplace

--- src/run_experiments.sh
+++ src/run_experiments.sh
@@ -4,7 +4,7 @@ set -e
 
 WEKA_CLASSPATH="C:/Program Files/Weka-3-6/weka.jar"
 TRAINING_SET_TEMPLATE=../data/pendigits/pendigits_randomized_training___X___pct.arff
-TEST_SET=../data/pendigits/pendigits_randomized_test_30pct.arff
+TEST_SET=../data/pendigits/pendigits_randomized_test_30_pct.arff
 
 while read config; do    
     for pct in 10 20 30 40 50 60 70; do
@@ -14,6 +14,6 @@ while read config; do
 
         echo $config, Training on $pct%, Output to $output_file
 
-        java -cp "$WEKA_CLASSPATH" $config -t $training_set -T $TEST_SET -d models/$model_file > results/$output_file
+        java -cp "$WEKA_CLASSPATH" $config -t $training_set -T $TEST_SET -d ../models/$model_file > ../results/$output_file
     done
 done < configs.txt
