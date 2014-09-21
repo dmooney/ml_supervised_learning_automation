9c0e45815bb171fc7ba19679416eec1c05e8d09b
David Mooney <dmooney@dmooney.org>
2014-09-21 10:21:57 -0400

    Restored laplace; Doesn't re-run completed runs

--- src/run_experiments.sh
+++ src/run_experiments.sh
@@ -9,11 +9,14 @@ TEST_SET=../data/pendigits/pendigits_randomized_test_30_pct.arff
 while read config; do    
     for pct in 10 20 30 40 50 60 70; do
         training_set=${TRAINING_SET_TEMPLATE/__X__/$pct}
-        output_file=${config// /_}_$pct.txt
-        model_file=${config// /_}_$pct.model
+        output_file=../results/${config// /_}_$pct.txt
+        model_file=../models/${config// /_}_$pct.model
 
-        echo $config, Training on $pct%, Output to $output_file
-
-        java -cp "$WEKA_CLASSPATH" $config -t $training_set -T $TEST_SET -d ../models/$model_file > ../results/$output_file
+        if [ ! -f $output_file ]; then
+        	echo $config, Training on $pct%, Output to $output_file
+	        java -cp "$WEKA_CLASSPATH" $config -t $training_set -T $TEST_SET -d $model_file > $output_file
+	    else
+	    	echo $config aleady run, skipping
+	    fi
     done
 done < configs.txt
